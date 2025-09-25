67f1b19bae93a56ab9f93f1ad8afd14977cdc3d2
oxxide216 <vie.devr@gmail.com>
2025-09-25 16:32:53 +0300

    Changed directories structure

--- build-vm.sh
+++ build-vm.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 
-CFLAGS="-Wall -Wextra -Ilib-vm-include -Iir-include"
+CFLAGS="-Wall -Wextra -Ivm-include -Iir-include"
 LDFLAGS="-z execstack -lX11 -lGL -lGLEW"
 BUILD_FLAGS="${@:1}"
-SRC="$(find vm-src -name "*.c")"
-LIB_SRC="$(find lib-vm-src -name "*.c" -not -name "io.c")"
+SRC="$(find test-vm-src -name "*.c")"
+LIB_SRC="$(find vm-src -name "*.c" -not -name "io.c")"
 IR_SRC="$(find ir-src -name "*.c" -not -name "serializer.*")"
 
 cc -o aether-vm $CFLAGS $LDFLAGS $BUILD_FLAGS $SRC $LIB_SRC $IR_SRC
