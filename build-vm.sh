709ad8ac61c16f6a8fb18b42b42eb3b8fca96db1
oxxide216 <vie.devr@gmail.com>
2025-09-25 16:04:02 +0300

    Fixed some compilation bugs

--- build-vm.sh
+++ build-vm.sh
@@ -4,7 +4,7 @@ CFLAGS="-Wall -Wextra -Ilib-vm-include -Iir-src"
 LDFLAGS="-z execstack -lX11 -lGL -lGLEW"
 BUILD_FLAGS="${@:1}"
 SRC="$(find vm-src -name "*.c")"
-LIB_SRC="$(find lib-vm-src -name "*.c")"
-IR_SRC="$(find ir-src -name "*.c" -not -name "serializer.*" -not -name "io.h")"
+LIB_SRC="$(find lib-vm-src -name "*.c" -not -name "io.c")"
+IR_SRC="$(find ir-src -name "*.c" -not -name "serializer.*")"
 
 cc -o aether-vm $CFLAGS $LDFLAGS $BUILD_FLAGS $SRC $LIB_SRC $IR_SRC
