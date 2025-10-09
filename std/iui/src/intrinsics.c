b88bdac22a211f1b4dba5602022823ec878fc448
oxxide216 <vie.devr@gmail.com>
2025-10-09 10:12:31 +0300

    Changed default button styling

--- std/iui/src/intrinsics.c
+++ std/iui/src/intrinsics.c
@@ -36,7 +36,7 @@ static void init_styles(Iui *iui) {
     vec4(0.0, 0.0, 0.0, 1.0),
     vec4(1.0, 1.0, 1.0, 1.0),
     vec4(0.0, 0.0, 0.0, 1.0),
-    vec4(1.0, 1.0, 1.0, 1.0),
+    vec4(0.5, 0.5, 0.5, 1.0),
   };
   DA_APPEND(iui->widgets.styles, button_style);
 
