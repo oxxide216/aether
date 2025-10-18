2c5cdb6291e002ca4f179fd563f50b5360045690
oxxide216 <vie.devr@gmail.com>
2025-10-18 18:25:55 +0300

    Fixed std paths

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 sudo cp aether /usr/bin/
 sudo rm -rf /usr/include/aether/
-sudo mkdir /usr/include/aether/
-sudo cp -r $(find std -name "*.ae") /usr/include/aether/
+sudo mkdir -p /usr/include/aether/std/
+sudo cp -r $(find std -name "*.ae") /usr/include/aether/std/
