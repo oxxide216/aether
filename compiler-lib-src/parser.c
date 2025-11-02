5270572aab2ffad7da652eae9d892b9ff13efd52
oxxide216 <vie.devr@gmail.com>
2025-11-02 17:33:13 +0300

    Added more escape characters

--- compiler-lib-src/parser.c
+++ compiler-lib-src/parser.c
@@ -71,8 +71,17 @@ static char escape_char(char _char) {
   case 'v': return '\v';
   case 'e': return '\e';
   case 'b': return '\b';
-  case '0': return '\0';
-  default:  return _char;
+  case '0': return 0;
+  case '1': return 1;
+  case '2': return 2;
+  case '3': return 3;
+  case '4': return 4;
+  case '5': return 5;
+  case '6': return 6;
+  case '7': return 7;
+  case '8': return 8;
+  case '9': return 9;
+  default: return _char;
   }
 }
 
