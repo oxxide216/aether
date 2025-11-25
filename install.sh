87c6f4e0a1d4a8d0a0e24294d9eb347c56004d65
oxxide216 <oxxide216@gmail.com>
2025-11-25 21:35:25 +0300

    Added repl

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ sudo cp aether /usr/bin/
 sudo rm -rf /usr/include/aether/
 sudo mkdir -p /usr/include/aether/std/
 sudo cp std/ae-src/* /usr/include/aether/std/
-sudo cp loader.ae /usr/include/aether/
-sudo cp ems-loader.ae /usr/include/aether/
+sudo cp loader.ae ems-loader.ae repl.ae /usr/include/aether/
