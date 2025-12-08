7786af22b3e7331eba81f5ac713b640f8521858b
oxxide216 <oxxide216@gmail.com>
2025-12-08 04:02:35 +0300

    Changed directories structure

--- build.sh
+++ build.sh
@@ -3,15 +3,14 @@
 # Compiler
 CC=cc
 OUT=aether
-CFLAGS="-Wall -Wextra -Icompiler-lib-include -Ivm-lib-include -Iir-include \
-        -Ilibs -Imisc -lm"
+CFLAGS="-Wall -Wextra -Iinclude -Ilibs -Isrc/misc \
+        -Ilibs/winx/include -Ilibs/glass/include -lm"
 BUILD_FLAGS="${@:1}"
 LDFLAGS="-z execstack"
-SRC="src/main.c"
-COMPILER_SRC="$(find compiler-lib-src -name "*.c")"
-VM_SRC="$(find vm-lib-src -name "*.c")"
-IR_SRC="$(find ir-src -name "*.c") $(find misc -name "*.c")"
-LIB_SRC="$(find libs/lexgen/runtime-src -name "*.c")"
+BIN_SRC="src/bin/main.c"
+MISC_SRC="$(find src/misc -name "*.c")"
+LIB_SRC="$(find src/lib -name "*.c")"
+LIBS_SRC="$(find libs/lexgen/runtime-src -name "*.c")"
 STD_SRC="src/std/core.c src/std/math.c src/std/str.c"
 
 if [ "$AETHER_GRAPHICS" == "" ]; then
@@ -28,24 +27,24 @@ fi
 
 if [ "$GLASS" != "" ]; then
   CFLAGS="$CFLAGS -DGLASS -Ilibs/winx/include -Ilibs/glass/include"
-  LIB_SRC="$LIB_SRC $(find libs/winx/src -name "*.c" -not -name "io.c" \
-                           -not -path "libs/winx/src/platform/*")"
-  LIB_SRC="$LIB_SRC $(find libs/glass/src -name "*.c" -not -name "io.c")"
+  LIBS_SRC="$LIB_SRC $(find libs/winx/src -name "*.c" -not -name "io.c" \
+                            -not -path "libs/winx/src/platform/*")"
+  LIBS_SRC="$LIB_SRC $(find libs/glass/src -name "*.c" -not -name "io.c")"
   STD_SRC="$STD_SRC src/std/glass/*"
   if [ "$AETHER_GRAPHICS" == "x11" ]; then
     CFLAGS="$CFLAGS -DX11"
     LDFLAGS="$LDFLAGS -lX11 -lGL -lGLEW"
-    LIB_SRC="$LIB_SRC $(find libs/winx/src/platform/x11 -name "*.c")"
+    LIBS_SRC="$LIB_SRC $(find libs/winx/src/platform/x11 -name "*.c")"
   elif [ "$AETHER_GRAPHICS" == "web" ]; then
     CFLAGS="$CFLAGS -DWEB"
-    LIB_SRC="$LIB_SRC $(find libs/winx/src/platform/web -name "*.c")"
+    LIBS_SRC="$LIB_SRC $(find libs/winx/src/platform/web -name "*.c")"
   fi
 fi
 
 lexgen compiler-lib-src/grammar.h compiler-lib-src/grammar.lg
 
-$CC -o $OUT $COMPILER_SRC $VM_SRC $IR_SRC $LIB_SRC \
-            $STD_SRC $SRC $CFLAGS $LDFLAGS $BUILD_FLAGS
+$CC -o $OUT $BIN_SRC $MISC_SRC $LIB_SRC $LIBS_SRC \
+            $STD_SRC $CFLAGS $LDFLAGS $BUILD_FLAGS
 
 if [ "$WASM" != "" ]; then
   rm -rf dest/
@@ -65,9 +64,9 @@ if [ "$WASM" != "" ]; then
                   -s USE_WEBGL2=1 -s USE_GLFW=0 \
                   -s ENVIRONMENT=web -s SINGLE_FILE=1"
   LDFLAGS=
-  SRC="src/emscripten-main.c"
+  BIN_SRC="src/emscripten-main.c"
   STD_SRC="$STD_SRC src/std/web.c"
 
-  $CC -o $OUT $COMPILER_SRC $VM_SRC $IR_SRC $LIB_SRC \
-              $STD_SRC $SRC $CFLAGS $LDFLAGS $BUILD_FLAGS
+  $CC -o $OUT $BIN_SRC $MISC_SRC $LIB_SRC $LIBS_SRC \
+              $STD_SRC $CFLAGS $LDFLAGS $BUILD_FLAGS
 fi
