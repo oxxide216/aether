778652a83794be6a249f5602d9c10bb16e56ceb0
oxxide216 <oxxide216@gmail.com>
2026-02-26 12:04:21 +0300

    Fixed TLS

--- src/std/tls/tlse-server.h
+++ src/std/tls/tlse-server.h
@@ -8,11 +8,18 @@
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wtype-limits"
+#define i32 _i32
 #define i64 _i64
+#define u32 _u32
 #define u64 _u64
 #include "tlse/tlse.c"
 #undef u64
+#undef u32
 #undef i64
+#undef i32
+#pragma GCC diagnostic pop
 #pragma GCC diagnostic pop
 #pragma GCC diagnostic pop
 #pragma GCC diagnostic pop
